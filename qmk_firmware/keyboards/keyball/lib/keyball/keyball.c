602c77618f041d3bbbd7d3a771b6b8d241b8fb2f
flatrock520 <s.hiraiwa0520@gmail.com>
2026-03-06 13:16:01 +0900

    Update keyball.c
    マウスキーを押した時AMLから離脱しない

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -710,6 +710,8 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
         keycode &= 0xff;
     }
 
+// disable AML getout when mouse key is pressed
+/*
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
     // reduce auto mouse timeout if mouse key is pressed.
     if ((is_mouse_record_kb(keycode, record) || IS_MOUSEKEY(keycode)) && record->event.pressed) {
@@ -717,6 +719,7 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
         keyball.total_mouse_movement = 0;
     }
 #endif
+*/
 
     switch (keycode) {
 #ifndef MOUSEKEY_ENABLE
